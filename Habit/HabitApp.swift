99cb5a64f80c6b4067cc119412e20e6ba67c4a84
SilasBorges <silasborgesc@hotmail.com>
2025-09-15 22:48:22 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Silas Borges on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
