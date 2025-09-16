4bb33dace5731b89b03838ef1226c30b741393be
SilasBorges <silasborgesc@hotmail.com>
2025-09-16 20:03:00 -0300

    Created structure basic

---
+++ Splash/View/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Habit
+//
+//  Created by Silas Borges on 16/09/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Ola")
+    }
+}
+
+#Preview {
+    SplashView()
+}
